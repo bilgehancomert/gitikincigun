6fd6a113017e375f3e1a8507763386d0ccd2b2f9
Bilgehan Comert <bilgehancomert@gmail.com>
2022-08-15 22:19:19 +0300

    V2

--- src/Day2GitGithub.java
+++ src/Day2GitGithub.java
@@ -14,6 +14,6 @@ public class Day2GitGithub {
     Not: Yukarıdaki iki komutu tek seferde kullandıktan sonra ikinci commit'lerim için sadece git push kullanırız
     Not: git log --oneline --> Commitlerimdeki yani versiyonlarımda ki durumu gösterir
     Not: Github ogrendim
+    6-) git pull --> Remote(yani uzak repo) dosylarımızı güncellemek için kullanılır
      */
-    }
 }
